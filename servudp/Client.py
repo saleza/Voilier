38576301915e9cf1e6b82a22c9fc160aa733adcb
Adrien <saleza@lacroixrouge-brest.fr>
2017-10-11 15:43:36 +0200

    Envoi de la latitude et longitude sans le - ok

--- servudp/Client.py
+++ servudp/Client.py
@@ -14,9 +14,25 @@ sock.sendto(trame, (UDP_IP, UDP_PORT))
 
 data, server = sock.recvfrom(13)
 trameretour = bytearray(data)
+
+
+lat3 = (float)(trameretour[5] << 24)
+lat2 = lat3 + (trameretour[6] << 16)
+lat1 = lat2 + (trameretour[7] << 8)
+lat0 = lat1 + (trameretour[8])
+lat = lat0 / 1000000
+
+longi3 = (float)(trameretour[9] << 24)
+longi2 = longi3 + (trameretour[10] << 16)
+longi1 = longi2 + (trameretour[11] << 8)
+longi0 = longi1 + (trameretour[12])
+longi = longi0 / 1000000
+
 print "-------Message renvoye------"
 print "ID :", trameretour[0]
 print "Longueur du datagram :", trameretour[1]
 print "Vitesse du vent :", trameretour[2]
 print "Direction du vent :", trameretour[3]
 print "Gite :", trameretour[4]
+print "Longitude : ", lat
+print "Latitude : ", longi
--- servudp/serveur_udp.py
+++ servudp/serveur_udp.py
@@ -1,21 +1,36 @@
 import socket
 
-<<<<<<< HEAD
 UDP_IP = "127.0.0.1"
-=======
-UDP_IP = "192.168.0.234"
->>>>>>> e54d20547cbe1c31f097f9b70f042067d7e3896a
 UDP_PORT = 6423
+Id = 5
+Taille = 8
+VitVent = 30
+DirVent = 6
+Gite = 4
+latitude = 24.548243
+longitude = 9.671498
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+
+latsansvirgule = int(latitude*1000000)
+lat3 = (latsansvirgule >> 24)
+lat2 = (latsansvirgule >> 16)&0xFF
+lat1 = (latsansvirgule >> 8)&0xFF
+lat0 = (latsansvirgule)&0xFF
+
+longisansvirgule = int(longitude*1000000)
+longi3 = (longisansvirgule >> 24)
+longi2 = (longisansvirgule >> 16)&0xFF
+longi1 = (longisansvirgule >> 8)&0xFF
+longi0 = (longisansvirgule)&0xFF
 
 print "Serveur Actif"
 
 while True:
     trame, addr = sock.recvfrom(4)
     data = bytearray(trame)
-    trameretour = bytearray([5,4,8,6,4])
+    trameretour = bytearray([Id,Taille,VitVent,DirVent,Gite,lat3,lat2,lat1,lat0,longi3,longi2,longi1,longi0])
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(trameretour, (addr[0], addr[1]))
     print '---------Message recu---------'
