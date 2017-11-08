52276ae4fc115d62fee1fe8f17b779532c31ff4e
Adrien <saleza@lacroixrouge-brest.fr>
2017-11-08 08:11:08 +0100

    Envoi de la latitude et longitude avec le - OK

--- servudp/Client.py
+++ servudp/Client.py
@@ -1,38 +1,43 @@
 import socket
-from TrameCli import Trame
+import sys
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 6423
+ID = 0
+Taille = 2
+Safran = 4
+GV = 5
 
-trame1 = Trame()
-tabtrame = trame1.tableauTrame()
-
-trame = bytearray(tabtrame)
-
+trame = bytearray([ID, Taille, Safran, GV])
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.sendto(trame, (UDP_IP, UDP_PORT))
 
 data, server = sock.recvfrom(13)
 trameretour = bytearray(data)
 
-
-lat3 = (float)(trameretour[5] << 24)
+lat3 = float(trameretour[5] << 24)
 lat2 = lat3 + (trameretour[6] << 16)
 lat1 = lat2 + (trameretour[7] << 8)
 lat0 = lat1 + (trameretour[8])
-lat = lat0 / 1000000
+latitude = lat0 / 1000000
 
-longi3 = (float)(trameretour[9] << 24)
+longi3 = (trameretour[9] << 24)
 longi2 = longi3 + (trameretour[10] << 16)
 longi1 = longi2 + (trameretour[11] << 8)
 longi0 = longi1 + (trameretour[12])
-longi = longi0 / 1000000
+longi = longi0
 
+if trameretour[9] > 127:
+    longi = (~longi) & 0xFFFFFFFF
+    longi = (longi+1) * -1
+
+longitude = longi * 0.000001
+  
 print "-------Message renvoye------"
-print "ID :", trameretour[0]
-print "Longueur du datagram :", trameretour[1]
-print "Vitesse du vent :", trameretour[2]
-print "Direction du vent :", trameretour[3]
-print "Gite :", trameretour[4]
-print "Longitude : ", lat
-print "Latitude : ", longi
+print "ID :", (trameretour[0])
+print "Longueur du datagram :", (trameretour[1])
+print "Vitesse du vent :", (trameretour[2])
+print "Direction du vent :", (trameretour[3])
+print "Gite :", (trameretour[4])
+print "Latitude :", (latitude)
+print "Longitude :", (longitude)
--- servudp/serveur_udp.py
+++ servudp/serveur_udp.py
@@ -7,20 +7,20 @@ Taille = 8
 VitVent = 30
 DirVent = 6
 Gite = 4
-latitude = 24.548243
-longitude = 9.671498
+latitude = 48.3389
+longitude = -17.04575
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 latsansvirgule = int(latitude*1000000)
-lat3 = (latsansvirgule >> 24)
+lat3 = (latsansvirgule >> 24)&0xFF
 lat2 = (latsansvirgule >> 16)&0xFF
 lat1 = (latsansvirgule >> 8)&0xFF
 lat0 = (latsansvirgule)&0xFF
 
 longisansvirgule = int(longitude*1000000)
-longi3 = (longisansvirgule >> 24)
+longi3 = (longisansvirgule >> 24)&0xFF
 longi2 = (longisansvirgule >> 16)&0xFF
 longi1 = (longisansvirgule >> 8)&0xFF
 longi0 = (longisansvirgule)&0xFF
